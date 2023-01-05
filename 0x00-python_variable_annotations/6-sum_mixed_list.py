fa57717ed5e570cf4f7fc997f5871b7845cdefd3
iamMAHAM <abdulkabore@gmail.com>
2023-01-05 08:07:20 +0000

    update list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+sums a mixed list module
+"""
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    """
+    sum a list of float
+    """
+    sum: float = 0
+    i = 0
+    for i in range(len(mxd_lst)):
+        sum += mxd_lst[i]
+
+    return sum
